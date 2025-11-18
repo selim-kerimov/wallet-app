d9f61916c34e5e7c5232b630744b9f164591a4bb
selim-kerimov <selimkerimow68@gmail.com>
2025-11-18 21:19:34 +0500

    added links to the /notifications screen

--- src/app/(tabs)/analytics.tsx
+++ src/app/(tabs)/analytics.tsx
@@ -1,10 +1,17 @@
 import { StyledText } from '@/shared/ui/StyledText'
+import { TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { router } from 'expo-router'
 
 export default function Screen() {
   return (
     <SafeAreaView>
-      <StyledText>Analytics</StyledText>
+      <TouchableOpacity
+        onPress={() => router.push('/notifications')}
+        style={{ flexDirection: 'row', alignItems: 'center', gap: 6 }}
+      >
+        <StyledText color="accent">Go to notifications screen</StyledText>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
--- src/app/(tabs)/payments.tsx
+++ src/app/(tabs)/payments.tsx
@@ -1,10 +1,17 @@
 import { StyledText } from '@/shared/ui/StyledText'
+import { router } from 'expo-router'
+import { TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Screen() {
   return (
     <SafeAreaView>
-      <StyledText>Payments</StyledText>
+      <TouchableOpacity
+        onPress={() => router.push('/notifications')}
+        style={{ flexDirection: 'row', alignItems: 'center', gap: 6 }}
+      >
+        <StyledText color="accent">Go to notifications screen</StyledText>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
