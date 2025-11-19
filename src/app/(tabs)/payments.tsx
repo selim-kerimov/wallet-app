9fa89902835e060c17e97bfd9c8b3d3b9436b99f
selim-kerimov <selimkerimow68@gmail.com>
2025-11-19 19:25:11 +0500

    liquid glass

--- src/app/(tabs)/payments.tsx
+++ src/app/(tabs)/payments.tsx
@@ -1,17 +1,53 @@
-import { StyledText } from '@/shared/ui/StyledText'
-import { router } from 'expo-router'
-import { TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { LiquidGlassView, isLiquidGlassSupported } from '@callstack/liquid-glass'
+import { StyleSheet, View } from 'react-native'
+import { StyledText } from '@/shared/ui/StyledText'
+import { Image } from 'expo-image'
 
 export default function Screen() {
   return (
-    <SafeAreaView>
-      <TouchableOpacity
-        onPress={() => router.push('/notifications')}
-        style={{ flexDirection: 'row', alignItems: 'center', gap: 6 }}
-      >
-        <StyledText color="accent">Go to notifications screen</StyledText>
-      </TouchableOpacity>
-    </SafeAreaView>
+    <View style={styles.main}>
+      <Image
+        source={require('@/assets/images/background.jpg')}
+        style={styles.backgroundImage}
+        contentFit="cover"
+      />
+
+      <SafeAreaView edges={['top']}>
+        <LiquidGlassView
+          style={[
+            styles.liquidGlass,
+            !isLiquidGlassSupported && { backgroundColor: 'rgba(255,255,255,0.5)' },
+          ]}
+          interactive
+          effect="clear"
+        >
+          <StyledText type="headlineMedium">Hello World</StyledText>
+        </LiquidGlassView>
+      </SafeAreaView>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  main: {
+    position: 'relative',
+    flex: 1,
+  },
+  liquidGlass: {
+    width: 200,
+    height: 100,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 16,
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: -1,
+  },
+})
